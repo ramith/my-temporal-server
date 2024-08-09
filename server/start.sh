8161d7708d8cd94df84bce2eea49c7e8f2127066
Ramith Jayasinghe <3402882+ramith@users.noreply.github.com>
2024-08-09 15:05:13 +0530

    more refactoring

--- server/start.sh
+++ server/start.sh
@@ -9,7 +9,7 @@ export PROMETHEUS_ENDPOINT=$TEMPORAL_BROADCAST_ADDRESS:8233
 sh ./check_mysql.sh
 
 # Set up databases using the provided auto-setup.sh script
-sh ./auto-setup.sh
+#sh ./auto-setup.sh
 
 # Start Temporal server
 sh ./entrypoint.sh
