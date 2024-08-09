4bad6d2e5bbbae678abb54464a64f752b3c253b5
Ramith Jayasinghe <3402882+ramith@users.noreply.github.com>
2024-08-09 17:52:25 +0530

    remove mysql verification

--- server/start.sh
+++ server/start.sh
@@ -6,7 +6,7 @@ export TEMPORAL_BROADCAST_ADDRESS=$(hostname -i)
 export PROMETHEUS_ENDPOINT=$TEMPORAL_BROADCAST_ADDRESS:8233
 
 # Check MySQL connectivity and database existence
-sh ./check_mysql.sh
+#sh ./check_mysql.sh
 
 # Set up databases using the provided auto-setup.sh script
 #sh ./auto-setup.sh
